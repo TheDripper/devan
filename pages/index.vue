7c57b460e1356f5d01901322ce499697da201b07
TheDripper <deadcustomer@Deads-MacBook-Air.local>
2021-07-31 16:05:53 -0700

    header footer

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,9 @@
 <template>
-  <div id="root" class="flex">
+  <div id="root" class="">
     <div :class="classes"></div>
+    <div id="header" v-html="header.content.rendered" class=""></div>
     <div id="content" v-html="page.content.rendered" class=""></div>
+    <div id="footer" v-html="footer.content.rendered" class=""></div>
   </div>
 </template>
 
@@ -45,6 +47,12 @@ export default {
     // page() {
     //   return this.$store.state.posts[0];
     // },
+    header() {
+      return this.$store.state.header;
+    },
+    footer() {
+      return this.$store.state.footer;
+    },
     classes() {
       return this.$store.state.classes;
     },
--- store/index.js
+++ store/index.js
@@ -1,11 +1,19 @@
 import pages from "~/static/pages.json";
 import classes from "~/static/classes.json";
 export const state = () => ({
+  header: '',
+  footer: '',
   posts: [],
   classes: "",
 });
 
 export const mutations = {
+  header(state, header) {
+    state.header = header;
+  },
+  footer(state, footer) {
+    state.footer = footer;
+  },
   posts(state, posts) {
     state.posts = posts;
   },
@@ -20,6 +28,10 @@ export const actions = {
   //    return page
   //  },
   async nuxtServerInit({ commit }) {
+    const header = await this.$axios.$get("/api/pages/177");
+    commit("header",header);
+    const footer = await this.$axios.$get("/api/pages/181");
+    commit("footer",footer);
     // if (process.env.NODE_ENV == "development") {
     //   const posts = await this.$axios.$get("/wp-json/wp/v2/pages");
     //   commit("posts", posts);
